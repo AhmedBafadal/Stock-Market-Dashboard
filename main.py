343ede3fe0f6609b6ac768cb5d027e90f0cb3cf5
Ahmed Bafadal <a.m.bafadal@gmail.com>
2018-08-06 21:46:00 +0100

    Added input box and basic callback

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
+from dash.dependencies import Input, Output, State
 
 
 app = dash.Dash()
@@ -10,8 +10,20 @@ app.layout = html.Div([
     html.H1('Stock Ticker Dashboard'),
     html.H3('Enter a stock symbol:'),
     dcc.Input(id='my_stock_picker',value='TSLA'),
-    dcc.Graph(id='my_graph',figure={'data':[{'x':[1,2],'y':[3,1]}]})
+    dcc.Graph(id='my_graph',figure={'data':[{'x':[1,2],'y':[3,1]}],
+    'layout':{'title':'Default Title'}
+    
+    
+    })
 ])
+
+@app.callback(Output('my_graph','figure'),[Input('my_stock_picker', 'value')])
+def update_graph(stock_ticker):
+    fig = {'data':[{'x':[1,2],'y':[3,1]}],
+    'layout':{'title':stock_ticker}}
+    return fig
+
+
 
 if __name__ == '__main__':
     app.run_server()
