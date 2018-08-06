2604d4251d8072586cf3c1abc17330d99048b850
Ahmed Bafadal <a.m.bafadal@gmail.com>
2018-08-06 22:46:45 +0100

    Added new files, dash state and pandas datepicker.

--- main.py
+++ main.py
@@ -2,25 +2,73 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-
+import pandas_datareader.data as web # requires v0.6.0 or later
+from datetime import datetime
+import pandas as pd
 
 app = dash.Dash()
 
+# Obtaining list of company names and symbols from csv
+nsdq = pd.read_csv('NASDAQcompanylist.csv')
+nsdq.set_index('Symbol', inplace=True)
+options = []
+for tic in nsdq.index:
+    options.append({'label':'{} {}'.format(tic,nsdq.loc[tic]['Name']), 'value':tic})
+
 app.layout = html.Div([
     html.H1('Stock Ticker Dashboard'),
-    html.H3('Enter a stock symbol:'),
-    dcc.Input(id='my_stock_picker',value='TSLA'),
-    dcc.Graph(id='my_graph',figure={'data':[{'x':[1,2],'y':[3,1]}],
-    'layout':{'title':'Default Title'}
-    
-    
-    })
+    html.Div([
+        html.H3('Select stock symbols:', style={'paddingRight':'30px'}),
+        dcc.Dropdown(
+            id='my_ticker_symbol',
+            options=options,
+            value=['AMD'],
+            multi=True
+        )
+    ], style={'display':'inline-block', 'verticalAlign':'top', 'width':'30%'}),
+    html.Div([
+        html.H3('Select start and end dates:'),
+        dcc.DatePickerRange(
+            id='my_date_picker',
+            min_date_allowed=datetime(2015, 1, 1),
+            max_date_allowed=datetime.today(),
+            start_date=datetime(2018, 1, 1),
+            end_date=datetime.today()
+        )
+    ], style={'display':'inline-block'}),
+    html.Div([
+        html.Button(
+            id='submit-button',
+            n_clicks=0,
+            children='Submit',
+            style={'fontSize':24, 'marginLeft':'30px'}
+        ),
+    ], style={'display':'inline-block'}),
+    dcc.Graph(
+        id='my_graph',
+        figure={
+            'data': [
+                {'x': [1,2], 'y': [3,1]}
+            ]
+        }
+    )
 ])
 
-@app.callback(Output('my_graph','figure'),[Input('my_stock_picker', 'value')])
-def update_graph(stock_ticker):
-    fig = {'data':[{'x':[1,2],'y':[3,1]}],
-    'layout':{'title':stock_ticker}}
+
+@app.callback(Output('my_graph', 'figure'), [Input('my_ticker_symbol', 'value'),Input('my_date_picker', 'start_date'),Input('my_date_picker', 'end_date')])
+def update_graph(n_clicks, stock_ticker, start_date, end_date):
+    # Getting the data (Need to reformat the data to be fed into the pandas datareader)
+    start = datetime.strptime(start_date[:10], '%Y-%m-%d')
+    end = datetime.strptime(end_date[:10], '%Y-%m-%d')
+    traces = []
+    for tic in stock_ticker:
+        # Passing in stock_ticker from the input value. 'iex' refers to the API code
+        df = web.DataReader(tic,'iex',start,end) # Dataframe of all the information with the historical stock price, index x: dates, y: closing stock price for the trading day
+        traces.append({'x':df.index, 'y': df.close, 'name':tic})
+    fig = {
+        'data': traces,
+        'layout': {'title':', '.join(stock_ticker)+' Closing Prices'}
+    }
     return fig
 
 
